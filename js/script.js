724d63611af292708077618f1ff29aae01f6c18d
MaxiAveiro <maxi.aveiro@gmail.com>
2022-06-24 21:49:47 -0300

    integrador v1.0

---
+++ js/script.js
@@ -0,0 +1,85 @@
+//Valor del ticket
+const ticket = 200;
+
+//Porcentajes de descuento segun categoria
+const descEstudiante = 80;
+const descTrainne = 50;
+const descJunior = 15;
+
+//Elementos en variables
+let nombre = document.getElementById('nombre');
+let apellido = document.getElementById('apellido');
+let correo = document.getElementById('correo');
+let cantidad = document.getElementById('cantidad');
+let categoria = document.getElementById('categoria');
+let total = document.getElementById('total');
+let resumen = document.getElementById('resumen');
+let reset = document.getElementById('reset');
+
+//Funcion para quitar el estilo error de las entradas del formulario
+function quitarClaseError(){
+    let elementos = document.querySelectorAll(".is-invalid");
+    for (let index = 0; index < elementos.length; index++) {
+        elementos[index].classList.remove("is-invalid");
+    }
+}
+
+//Validar email
+function validateEmail(email) {
+    const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return res.test(String(email).toLowerCase());
+}
+
+//Calcular el total a pagar
+function calcularTotal(){    
+
+    quitarClaseError();
+
+    //validaciones de los valores
+    if(nombre.value === ""){
+        nombre.classList.add('is-invalid');
+        return;
+    } 
+
+    if(apellido.value === ""){
+        apellido.classList.add('is-invalid');
+        return;
+    } 
+
+    if(correo.value === "" || !validateEmail(correo.value)){
+        correo.classList.add('is-invalid');
+        return;
+    } 
+
+    if(cantidad.value === ""){
+        cantidad.classList.add('is-invalid');
+        return;
+    } 
+
+    let cant = Number(cantidad.value);
+    let cat = categoria.value;
+    let descuento = 0;
+    switch (cat) {
+        case '1':
+            descuento = descEstudiante;
+            break;
+        case '2':
+            descuento = descTrainne;
+            break;
+        case '3':
+            descuento = descJunior;
+            break;
+    }
+
+    let calculo = ticket * cant * ((100 - descuento)/100);
+    total.innerHTML = calculo;
+}
+
+function resetear(){
+    quitarClaseError();
+    total.innerHTML = '';
+}
+
+resumen.addEventListener('click', calcularTotal);
+
+reset.addEventListener('click', resetear);
